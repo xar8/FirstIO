9b2cebe3d47361e722bf1b3cb462f1250fd68b8f
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-09-05 17:17:49 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+//Author: Jason Evans
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Jason. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
